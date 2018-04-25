c137f09f2299f182368ffca049ada735019f3679
Unknown <blukmays@gmail.com>
2018-04-25 11:48:32 -0600

    parse

--- index.js
+++ index.js
@@ -2,11 +2,8 @@ const fs = require('fs');
 const simpleParser = require('mailparser').simpleParser;
 const createCsvWriter = require('csv-writer').createObjectCsvWriter;
 
-//
-// This function will read a file from disk and return it
-// as a string. The Path argument should be the exact path
-// to the file to read
-//
+
+
 async function fileLoad(path) {
 
   return new Promise(resolve => {
@@ -21,10 +18,7 @@ async function fileLoad(path) {
 
 }
 
-//
-// This function takes a string that represents an email message
-// in mime format and returns the parsed message as an object.
-//
+
 async function parseMail(contents) {
 
   return new Promise((resolve) => {
@@ -39,10 +33,8 @@ async function parseMail(contents) {
 
 }
 
-//
-// This function takes a path to a directory and will return
-// an array of all files within.
-//
+
+
 async function fileListDirectory(path) {
 
   return new Promise(resolve => {
@@ -57,22 +49,15 @@ async function fileListDirectory(path) {
 
 }
 
-//
-// Main program
-//
+
 async function run() {
 
-  //
-  // First we need to get a list of the message files.
-  //
+
   const files = await fileListDirectory('messages')
 
   let messages = [];
 
-  //
-  // Now that we have a list of message files we are going
-  // to loop through each file and parse it.
-  //
+
   for (let i = 0; i < files.length; i++) {
 
     const message = await parseMail(await fileLoad(`messages/${files[i]}`));
@@ -88,11 +73,7 @@ async function run() {
     console.log(`from: ${from}`)
     console.log(`subject: ${subject}`)
 
-    //
-    // Take the processed message object and add it
-    // to an array that will be wrintten to a csv file
-    // below.
-    //
+
     messages.push({
 
       date: message.date,
@@ -104,9 +85,7 @@ async function run() {
 
   }
 
-  //
-  // Write array of messages to csv file.
-  //
+
   const csvWriter = createCsvWriter({
 
     path: 'output.csv',
@@ -127,7 +106,5 @@ async function run() {
 
 }
 
-//
-// Start the program
-//
+
 run()
